378cef84c515489a62eb0acebe09aeff1db132b4
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-09 07:36:01 +0800

    Rendering Workout Input Form

--- script.js
+++ script.js
@@ -21,18 +21,19 @@ const inputCadence = document.querySelector(
 const inputElevation = document.querySelector(
   '.form__input--elevation'
 );
-
+let map, mapEvent;
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(
-        `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
+        `https://www.google.com/maps/@${latitude},${longitude}`
       );
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
+
       // console.log(map);
 
       L.tileLayer(
@@ -43,25 +44,49 @@ if (navigator.geolocation)
         }
       ).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+      // Handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
       alert('Could not get your position');
     }
   );
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  //Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation
+    .closest('.form__row')
+    .classList.toggle('form__row--hidden');
+  inputCadence
+    .closest('.form__row')
+    .classList.toggle('form__row--hidden');
+});
