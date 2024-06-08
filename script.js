d09a2445c5ad31e416bc21b2382c6e4ea3389a6c
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-06-09 06:14:24 +0800

    Displaying a Map using leaflet library

--- script.js
+++ script.js
@@ -4,12 +4,23 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const containerWorkouts =
+  document.querySelector('.workouts');
+const inputType = document.querySelector(
+  '.form__input--type'
+);
+const inputDistance = document.querySelector(
+  '.form__input--distance'
+);
+const inputDuration = document.querySelector(
+  '.form__input--duration'
+);
+const inputCadence = document.querySelector(
+  '.form__input--cadence'
+);
+const inputElevation = document.querySelector(
+  '.form__input--elevation'
+);
 
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
@@ -19,6 +30,27 @@ if (navigator.geolocation)
       console.log(
         `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`
       );
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 17);
+      console.log(map);
+
+      L.tileLayer(
+        'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }
+      ).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup(
+          'A pretty CSS popup.<br> Easily customizable.'
+        )
+        .openPopup();
+
+      // map.on();
     },
     function () {
       alert('Could not get your position');
